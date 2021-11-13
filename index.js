1c13cf3568efbb2afc177bcdea7e4c7657637045
ragibBarket317 <ragibrahman86317@gmail.com>
2021-11-14 03:06:57 +0600

    get system added

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express')
-const app = express()
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
 const cors = require('cors')
+const app = express()
 const port = process.env.PORT || 5000;
 
 
 //midleware
 app.use(cors())
 app.use(express.json())
+
+//DATABASE CONECTION
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.y5uft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('buy_bycycle');
+        const cycleCollection = database.collection('cycles');
+
+        //Get Cycles
+        app.get('/cycles', async (req, res) => {
+            const cursor = cycleCollection.find({});
+            const cycles = await cursor.toArray();
+            res.send(cycles);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
